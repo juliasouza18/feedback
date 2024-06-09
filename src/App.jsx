9ff2898a85bfbbd47f6d98c8e070219881bffdbf
Julia Schiavi <julia.souza18@eaportal.org>
2024-06-09 20:42:03 +0000

    criação dos estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 
-import {Home} from "./pages/Home"
-import {theme} from "./styles/theme.js"
+import { Home } from "./pages/Home"
+import { theme } from "./styles/theme.js"
+import { GlobalStyle } from "./styles/global.js"
 
-const router= createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
-    element:<Home />
+    element: <Home/>
   }
 ])
-export  function App() {
+
+export function App() {
   return (
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
