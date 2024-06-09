37cfdd92efa1e0094fe50446c0a460ed5873e35d
Julia Schiavi <julia.souza18@eaportal.org>
2024-06-09 22:53:39 +0000

    feat: adição da verificação da nota e do envio da nota para mudar de pagina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import { ButtonContainer, CardContainer, IconContainer, ResultContainer  } from "./Home.styles";
 import iconStarImg from "../assets/icon-star-svg"
 import thankYouImg from "../assets/illustration-thank-you-svg"
+import { useState } from "react";
 
 export  function Home() {
 
-  let aparecerResultado =true
+  const [mostrarResultado, setMostrarResultado] = useState(false)
+  const [feedbackNote, setFeedback] = useState(0)
+
+  function handFeedbackButtonClick(event) {
+    const feedback = Number(event.terget.innerText)
+
+    setFeedbackNote(feedback)
+
+  }
+
+  function handleSubmit() {
+    if (feedbackNote===0) return
+
+    setMostrarResultado(true)
+  }
+
+
   return (
-    aparecerResultado === false ? (
+    mostrarResultado === false ? (
       <CardContainer>
       <IconContainer>
         <img src={iconStarImg} alt="ícone de estrela" />
@@ -16,21 +33,21 @@ export  function Home() {
 
 
       <ButtonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handFeedbackButtonClick}>1</button>
+        <button onClick={handFeedbackButtonClick}>2</button>
+        <button onClick={handFeedbackButtonClick}>3</button>
+        <button onClick={handFeedbackButtonClick}>4</button>
+        <button onClick={handFeedbackButtonClick}>5</button>
       </ButtonContainer>
 
-      <button>Enviar</button>
+      <button onClick={handleSubmit}>Enviar</button>
     </CardContainer>
     ) :(
       <CardContainer>
         <img src={thankYouImg} alt="imagem de agradecimento" />
 
         <ResultContainer>
-          <p>Você selecionou 4 de 5</p>
+          <p>Você selecionou {feedbackNote} de 5</p>
         </ResultContainer>
 
         <h1>Obrigado!</h1>
